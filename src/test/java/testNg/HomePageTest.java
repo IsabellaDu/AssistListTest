1cfd4128d63d1bc67c391bef3f2a5dc745aa959b
izabelladudaryk <izabelladudaryk@gmail.com>
2023-04-28 14:41:22 -0700

    Rename BasePageTest to HomePageTest

--- src/test/java/testNg/HomePageTest.java
+++ src/test/java/testNg/HomePageTest.java
@@ -3,7 +3,7 @@ package testNg;
 import ca.assistlist.pages.HomePage;
 import org.testng.annotations.Test;
 
-public class BasePageTest extends BaseTest {
+public class HomePageTest extends BaseTest {
     @Test
     public void openHomePage() {
         HomePage homePage = new HomePage(driver);
